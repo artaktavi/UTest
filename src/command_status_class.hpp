29ba1e2afe89ef4fd8b7dc52caa7028363bb1149
Artyom Khafizov <khafizov.ar@phystech.edu>
2024-03-12 19:58:57 +0300

    fixed type of 'line' in command status class

--- src/command_status_class.hpp
+++ src/command_status_class.hpp
@@ -5,20 +5,19 @@
 
 struct CommandStatus {
   std::string type;
-  std::string line;
+  int line;
   std::string arg_1;
   std::string arg_2;
   std::string result;
-  CommandStatus(const std::string& c_type, const std::string& c_line,
-                const std::string& c_arg_1, const std::string& c_arg_2,
-                const std::string& c_result)
+  CommandStatus(std::string c_type, int c_line, std::string c_arg_1,
+                std::string c_arg_2, std::string c_result)
       : type(c_type),
         line(c_line),
         arg_1(c_arg_1),
         arg_2(c_arg_2),
         result(c_result) {}
-  CommandStatus(const std::string& c_type, const std::string& c_line,
-                const std::string& c_arg_1, const std::string& c_result)
+  CommandStatus(std::string c_type, int c_line, std::string c_arg_1,
+                std::string c_result)
       : type(c_type), line(c_line), arg_1(c_arg_1), result(c_result) {}
 };
 
