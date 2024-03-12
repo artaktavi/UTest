d7a1a5f43efa2ecca1fa07bf7efc0bb23144f4d3
Artyom Khafizov <khafizov.ar@phystech.edu>
2024-03-12 17:35:59 +0300

    added invoking tests by groups

--- src/test_define.hpp
+++ src/test_define.hpp
@@ -1,18 +1,18 @@
 #ifndef UNITTESTER_TEST_DEFINE_HPP
 #define UNITTESTER_TEST_DEFINE_HPP
 
-#include "test_registry.hpp"
+#include "test_registry_class.hpp"
 
-#define TEST(test_name, test_group)                     \
-  class test_name : public Test {                       \
-   public:                                              \
-    test_name() {                                       \
-      TestRegistry::AddTest(#test_name, #test_group,    \
-                            dynamic_cast<Test*>(this)); \
-    }                                                   \
-    void TestBody() override;                           \
-  };                                                    \
-  test_name Test##test_name;                            \
+#define TEST(test_name, test_group)                          \
+  class test_name : public Test {                            \
+   public:                                                   \
+    test_name() {                                            \
+      TestRegistry::RegisterTest(#test_name, #test_group,    \
+                                 dynamic_cast<Test*>(this)); \
+    }                                                        \
+    void TestBody() override;                                \
+  };                                                         \
+  test_name Test##test_name;                                 \
   void test_name::TestBody()
 
 #endif  //UNITTESTER_TEST_DEFINE_HPP
---
+++ src/test_registry_class.hpp
@@ -0,0 +1,41 @@
+#ifndef UNITTESTER_TEST_REGISTRY_CLASS_HPP
+#define UNITTESTER_TEST_REGISTRY_CLASS_HPP
+
+#include <string>
+#include <unordered_map>
+#include "test_class.hpp"
+
+class TestRegistry {
+ public:
+  static std::unordered_map<std::string, std::unordered_map<std::string, Test*>>
+      test_groups_;
+  static std::unordered_map<std::string, Test*> test_all_;
+
+  static void RegisterTest(const std::string& test_name,
+                           const std::string& test_group, Test* test) {
+    //    !!!!
+    //    add checking if test already there is by this address
+    //    !!!!
+    test_groups_[test_group][test_name] = test;
+    test_all_[test_name] = test;
+  }
+  static void ExecuteTest(const std::string& test_name) noexcept {
+    test_all_[test_name]->Execute();
+  }
+  static void ExecuteTestGroup(const std::string& test_group) noexcept {
+    for (std::pair<std::string, Test*> curr_test : test_groups_[test_group]) {
+      curr_test.second->Execute();
+    }
+  }
+  static void ExecuteTestAll() noexcept {
+    for (std::pair<std::string, Test*> curr_test : test_all_) {
+      curr_test.second->Execute();
+    }
+  }
+};
+
+std::unordered_map<std::string, std::unordered_map<std::string, Test*>>
+    TestRegistry::test_groups_;
+std::unordered_map<std::string, Test*> TestRegistry::test_all_;
+
+#endif  //UNITTESTER_TEST_REGISTRY_CLASS_HPP
