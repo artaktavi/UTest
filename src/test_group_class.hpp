599336329875750694665e69a6a4b12e9acdb0bb
Artyom Khafizov <khafizov.ar@phystech.edu>
2024-03-14 18:19:06 +0300

    added test group class and modified test registry

---
+++ src/test_group_class.hpp
@@ -0,0 +1,39 @@
+#ifndef UNITTESTER_TEST_GROUP_CLASS_HPP
+#define UNITTESTER_TEST_GROUP_CLASS_HPP
+
+#include "test_class.hpp"
+#include "test_group_status_class.hpp"
+
+#include <vector>
+
+class TestGroup {
+ private:
+  std::string group_name_;
+  std::vector<Test*> tests_;
+  std::vector<TestStatus> tests_history_;
+  bool is_passed_ = true;
+
+ public:
+  TestGroup() = delete;
+  TestGroup(const TestGroup& other) = default;
+  TestGroup(std::string name) : group_name_(name) {}
+  void AddTest(Test* test) { tests_.push_back(test); }
+  TestGroupStatus Execute() {
+    tests_history_.clear();
+    is_passed_ = true;
+    const auto start_time = std::chrono::high_resolution_clock::now();
+    for (Test* test : tests_) {
+      tests_history_.push_back(test->Execute());
+      if (tests_history_.back().result == "failed") {
+        is_passed_ = false;
+      }
+    }
+    const auto stop_time = std::chrono::high_resolution_clock::now();
+    const std::chrono::duration<double> group_execution_time =
+        stop_time - start_time;
+    return {group_name_, tests_history_, group_execution_time,
+            is_passed_ ? "succeed" : "failed"};
+  }
+};
+
+#endif  //UNITTESTER_TEST_GROUP_CLASS_HPP
--- src/test_registry_class.hpp
+++ src/test_registry_class.hpp
@@ -5,53 +5,49 @@
 #include <string>
 #include <unordered_map>
 #include "test_class.hpp"
+#include "test_group_class.hpp"
 
 class TestRegistry {
  public:
-  static std::unordered_map<std::string, std::unordered_map<std::string, Test*>>
-      test_groups_;
+  static std::unordered_map<std::string, TestGroup> test_groups_;
   static std::unordered_map<std::string, Test*> test_all_;
 
-  static void RegisterTest(const std::string& test_name,
-                           const std::string& test_group, Test* test) {
+  static void RegisterTest(const std::string& name,
+                           const std::string& group_name, Test* test) {
     //    !!!!
     //    add checking if test already there is by this address
     //    !!!!
-    test_groups_[test_group][test_name] = test;
-    test_all_[test_name] = test;
+    if (test_groups_.find(group_name) == test_groups_.end()) {
+      test_groups_.insert({group_name, TestGroup(group_name)});
+    }
+    test_groups_.find(group_name)->second.AddTest(test);
+    test_all_[name] = test;
   }
   static TestStatus ExecuteTest(const std::string& test_name,
                                 bool detailed = false) noexcept {
     return test_all_[test_name]->Execute();
   }
-  static std::vector<TestStatus> ExecuteTestGroup(
-      const std::string& test_group, bool detailed = false) noexcept {
-    std::vector<TestStatus> tests_result;
-    tests_result.reserve(test_groups_[test_group].size());
-    for (std::pair<std::string, Test*> curr_test : test_groups_[test_group]) {
-      tests_result.push_back(curr_test.second->Execute());
+  static TestGroupStatus ExecuteTestGroup(const std::string& group_name,
+                                          bool detailed = false) noexcept {
+    if (test_groups_.find(group_name) != test_groups_.end()) {
+      return test_groups_.find(group_name)->second.Execute();
+    } else {
+      std::cerr << "ExecuteTestGroup: there is no groups known as "
+                << group_name << std::endl;
     }
-    return tests_result;
   }
-  static std::vector<TestStatus> ExecuteTestAll(
+  static std::vector<TestGroupStatus> ExecuteTestAll(
       bool detailed = false) noexcept {
-    std::vector<TestStatus> tests_result;
-    tests_result.reserve(test_all_.size());
-    for (std::pair<std::string, Test*> curr_test : test_all_) {
-      tests_result.push_back(curr_test.second->Execute());
-      //      std::cout << "TEST: " << curr_test.first << " "
-      //                << tests_result.back().result << " with "
-      //                << tests_result.back().execution_time.count() << "s\n";
-      // OR to customize time ratio:
-      //                << std::chrono::duration_cast<std::chrono::microseconds>(
-      //                       curr_test_res.execution_time).count() << "micros\n";
+    std::vector<TestGroupStatus> tests_result;
+    tests_result.reserve(test_groups_.size());
+    for (std::pair<const std::string, TestGroup>& curr_group : test_groups_) {
+      tests_result.push_back(curr_group.second.Execute());
     }
     return tests_result;
   }
 };
 
-std::unordered_map<std::string, std::unordered_map<std::string, Test*>>
-    TestRegistry::test_groups_;
+std::unordered_map<std::string, TestGroup> TestRegistry::test_groups_;
 std::unordered_map<std::string, Test*> TestRegistry::test_all_;
 
 #endif  //UNITTESTER_TEST_REGISTRY_CLASS_HPP
