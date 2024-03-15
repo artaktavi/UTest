a8b7dd6fe00c54357043fb20bae172d73a2bcd06
Artyom Khafizov <khafizov.ar@phystech.edu>
2024-03-16 01:45:28 +0300

    added json serialization to io_manager class

--- src/test_io_manager_class.hpp
+++ src/test_io_manager_class.hpp
@@ -55,11 +55,36 @@ class TestIOManager {
   static void SetOutputJsonFilePath(const std::string& new_path) noexcept {
     out_jsonfile_path_ = new_path;
   }
-  static void SerializeToJsonFile(
-      const std::string& path,
-      const std::vector<TestGroupStatus>& test_groups) {}
+  static void SerializeToJsonFile(const std::string& path,
+      const std::vector<TestGroupStatus>& test_groups) {
+    nlohmann::json new_json;
+    for (const TestGroupStatus& group : test_groups) {
+      new_json[group.group_name]["res"] = group.result;
+      new_json[group.group_name]["exec"] = group.group_execution_time.count();
+      for (const TestStatus& test : group.tests_history) {
+        new_json[group.group_name]["tests"][test.name]["res"] = test.result;
+        new_json[group.group_name]["tests"][test.name]["exec"] = test.execution_time.count();
+        for (const CommandStatus& command : test.commands_history) {
+          new_json[group.group_name]["tests"][test.name]["commands"][std::to_string(command.line)]["type"] = command.type;
+          new_json[group.group_name]["tests"][test.name]["commands"][std::to_string(command.line)]["res"] = command.result;
+          new_json[group.group_name]["tests"][test.name]["commands"][std::to_string(command.line)]["arg_1"] = command.arg_1;
+          new_json[group.group_name]["tests"][test.name]["commands"][std::to_string(command.line)]["arg_2"] = command.arg_2;
+        }
+      }
+    }
+    std::ofstream new_file(path);
+    new_file << new_json;
+    new_file.close();
+  }
   static std::vector<TestGroupStatus> DeserializeFromJsonFile(
-      const std::string& json_file_path) noexcept {}
+      const std::string& json_file_path) noexcept {
+    
+  }
 };
+
+bool TestIOManager::O_STREAM_ENABLED = true;
+bool TestIOManager::O_CERR_STREAM_ENABLED = false;
+bool TestIOManager::O_JSONFILE_ENABLED = false;
+std::string TestIOManager::out_jsonfile_path_ = "tests_result.json";
 
 #endif  //UNITTESTER_TEST_IO_MANAGER_CLASS_HPP
