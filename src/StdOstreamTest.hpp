2ab19630909a464ad6af59fc6330a062d72df9a4
Artyom Khafizov <khafizov.ar@phystech.edu>
2024-04-03 15:22:00 +0300

    divided and pulled output functions to particular namespace

--- src/StdOstreamTest.hpp
+++ src/StdOstreamTest.hpp
@@ -1,22 +1,45 @@
 #pragma once
 
+#include <OutputConfig.hpp>
 #include <ConsoleSettings.hpp>
 #include <TestResult.hpp>
 #include <iomanip>
 
-std::ostream& operator<<(std::ostream& o_stream,
-                         const CommandStatus& command_status) {
+namespace StdOstreamTest {
+void DisplayTestStartOstream(std::ostream& o_stream, const std::string& group,
+                             const std::string& name) {
+  o_stream << "  |  "
+           << ConsoleSettings::GetColorCode(ConsoleSettings::common_color)
+           << " [ " << std::setw(6) << std::left << KEYWORD_TEST_START << " ] "
+           << group << '.' << name << ConsoleSettings::GetColorCode("reset")
+           << std::endl;
+}
+void DisplayGroupStartOstream(std::ostream& o_stream,
+                              const std::string& group_name) {
+  o_stream << ConsoleSettings::GetColorCode(ConsoleSettings::common_color)
+           << "[ --- " << group_name << " --- ] "
+           << "[ v ======================= ]\n"
+           << ConsoleSettings::GetColorCode("reset");
+  //  o_stream << "[ | ======================= ]\n";
+}
+void DisplayCommandResultOstream(std::ostream& o_stream,
+                                 const CommandStatus& command_status) {
   o_stream << command_status.line << ": " << command_status.type << '('
            << command_status.arg_1;
   if (!command_status.arg_2.empty()) {
     o_stream << ", " << command_status.arg_2;
   }
   o_stream << ")  :  " << command_status.result << '\n';
-  return o_stream;
 }
-
-std::ostream& operator<<(std::ostream& o_stream,
-                         const TestStatus& test_status) {
+void DisplayTestResultOstream(std::ostream& o_stream,
+                              const TestStatus& test_status) {
+  if (OutputConfig::is_always_detailed ||
+      (OutputConfig::is_failed_detailed &&
+       test_status.result != KEYWORD_PASSED)) {
+    for (const CommandStatus& command : test_status.commands_history) {
+      DisplayCommandResultOstream(o_stream, command);
+    }
+  }
   o_stream << "  |  ";
   if (test_status.result == KEYWORD_PASSED) {
     o_stream << ConsoleSettings::GetColorCode(
@@ -25,27 +48,41 @@ std::ostream& operator<<(std::ostream& o_stream,
     o_stream << ConsoleSettings::GetColorCode(
         ConsoleSettings::test_failed_color);
   }
-  o_stream << " [ " << std::setw(6) << std::right << test_status.result
-           << " ] " << ConsoleSettings::GetColorCode("reset");
+  o_stream << " [ " << std::setw(6) << std::right << test_status.result << " ] "
+           << ConsoleSettings::GetColorCode("reset");
   o_stream << " time: " << test_status.execution_time.count() << "s\n";
-  if (ConsoleSettings::is_always_detailed ||
-      (ConsoleSettings::is_failed_detailed &&
-       test_status.result != KEYWORD_PASSED)) {
-    for (const CommandStatus& command : test_status.commands_history) {
-      o_stream << command;
-    }
+}
+void DisplayGroupResultOstream(std::ostream& o_stream,
+                               const TestGroupStatus& group_status) {
+  o_stream << ConsoleSettings::GetColorCode("reset");
+  o_stream << "[ ^ ======================= ]\n\n";
+}
+}  // namespace StdOstreamTest
+
+std::ostream& operator<<(std::ostream& o_stream,
+                         const CommandStatus& command_status) {
+  StdOstreamTest::DisplayCommandResultOstream(o_stream, command_status);
+  return o_stream;
+}
+
+std::ostream& operator<<(std::ostream& o_stream,
+                         const TestStatus& test_status) {
+  StdOstreamTest::DisplayTestStartOstream(o_stream, test_status.group_name,
+                                          test_status.name);
+  for (const CommandStatus& command : test_status.commands_history) {
+    o_stream << command;
   }
+  StdOstreamTest::DisplayTestResultOstream(o_stream, test_status);
   return o_stream;
 }
 
 std::ostream& operator<<(std::ostream& o_stream,
                          const TestGroupStatus& group_status) {
-
-  o_stream << ConsoleSettings::GetColorCode("reset");
+  StdOstreamTest::DisplayGroupStartOstream(o_stream, group_status.group_name);
   for (const TestStatus& test : group_status.tests_history) {
     o_stream << test;
   }
-  o_stream << "[ ^ ======================= ]\n\n";
+  StdOstreamTest::DisplayGroupResultOstream(o_stream, group_status);
   return o_stream;
 }
 
--- src/TestIOManager.hpp
+++ src/TestIOManager.hpp
@@ -3,30 +3,21 @@
 #include <StdOstreamTest.hpp>
 
 class TestIOManager {
-  static bool o_stream_enabled_;
-  static bool o_cerr_stream_enabled_;
-  static bool o_json_file_enabled_;
   static TestResult general_test_output_;
 
  public:
-//  static void SetCoutConsoleColor(const std::string& color) noexcept {
-//
-//  }
-//  static void SetCerrConsoleColor(const std::string& color) noexcept {
-//
-//  }
   static void ToggleOStream() noexcept {
-    o_stream_enabled_ = !o_stream_enabled_;
+    OutputConfig::o_cout_stream_enabled = !OutputConfig::o_cout_stream_enabled;
   }
   static void ToggleOCerrStream() noexcept {
-    o_cerr_stream_enabled_ = !o_cerr_stream_enabled_;
+    OutputConfig::o_cerr_stream_enabled = !OutputConfig::o_cerr_stream_enabled;
   }
   static void SwitchStreams() noexcept {
     ToggleOStream();
     ToggleOCerrStream();
   }
   static void ToggleOJsonFile(const std::string& new_path = "") noexcept {
-    o_json_file_enabled_ = !o_json_file_enabled_;
+    OutputConfig::o_json_file_enabled = !OutputConfig::o_json_file_enabled;
     if (!new_path.empty()) {
       SetOutputJsonFilePath(new_path);
     }
@@ -35,70 +26,56 @@ class TestIOManager {
     }
   }
   static bool IsOStreamEnabled() noexcept {
-    return o_stream_enabled_;
+    return OutputConfig::o_cout_stream_enabled;
   }
   static bool IsOCerrStreamEnabled() noexcept {
-    return o_cerr_stream_enabled_;
+    return OutputConfig::o_cerr_stream_enabled;
   }
   static bool IsOJsonFileEnabled() noexcept {
-    return o_json_file_enabled_;
+    return OutputConfig::o_json_file_enabled;
   }
   static void SetOutputJsonFilePath(const std::string& new_path) noexcept {
     general_test_output_.SetPathToAutoSave(new_path);
   }
-  static void OutputTestStatus(const TestStatus& test_status) {
-    if (o_stream_enabled_) {
-      std::cout << test_status;
+  static void OutputTestResult(const TestStatus& test_status) {
+    if (OutputConfig::o_cout_stream_enabled) {
+      StdOstreamTest::DisplayTestResultOstream(std::cout, test_status);
     }
-    if (o_cerr_stream_enabled_) {
-      std::cerr << test_status;
+    if (OutputConfig::o_cerr_stream_enabled) {
+      StdOstreamTest::DisplayTestResultOstream(std::cerr, test_status);
     }
-    if (o_json_file_enabled_) {
+    if (OutputConfig::o_json_file_enabled) {
       general_test_output_.AddTestStatus(test_status);
     }
   }
-  static void OutputTestStart(const std::string& group, const std::string& name) {
-    if (o_stream_enabled_) {
-      std::cout << "  |  "
-               << ConsoleSettings::GetColorCode(ConsoleSettings::common_color)
-               << " [ " << std::setw(6) << std::left << KEYWORD_TEST_START
-               << " ] " << group << '.' << name
-               << ConsoleSettings::GetColorCode("reset") << std::endl;
+  static void OutputTestStart(const std::string& group,
+                              const std::string& name) {
+    if (OutputConfig::o_cout_stream_enabled) {
+      StdOstreamTest::DisplayTestStartOstream(std::cout, group, name);
     }
-    if (o_cerr_stream_enabled_) {
-      std::cerr << "  |  "
-               << ConsoleSettings::GetColorCode(ConsoleSettings::common_color)
-               << " [ " << std::setw(6) << std::left << KEYWORD_TEST_START
-               << " ] " << group << '.' << name
-               << ConsoleSettings::GetColorCode("reset") << std::endl;
+    if (OutputConfig::o_cerr_stream_enabled) {
+      StdOstreamTest::DisplayTestStartOstream(std::cerr, group, name);
     }
   }
-  static void OutputGroupStatus(const TestGroupStatus& group_status) {
-    if (o_stream_enabled_) {
-      std::cout << group_status;
+  static void OutputGroupResult(const TestGroupStatus& group_status) {
+    if (OutputConfig::o_cout_stream_enabled) {
+      StdOstreamTest::DisplayGroupResultOstream(std::cout, group_status);
     }
-    if (o_cerr_stream_enabled_) {
-      std::cerr << group_status;
+    if (OutputConfig::o_cerr_stream_enabled) {
+      StdOstreamTest::DisplayGroupResultOstream(std::cerr, group_status);
     }
-    if (o_json_file_enabled_) {
+    if (OutputConfig::o_json_file_enabled) {
       general_test_output_.AddGroupStatus(group_status);
     }
   }
   static void OutputGroupStart(const std::string& group_name) {
-    if (o_stream_enabled_) {
-      std::cout << ConsoleSettings::GetColorCode(ConsoleSettings::common_color) << "[ --- " << group_name
-               << " --- ] " << "[ v ======================= ]\n";
-      //  o_stream << "[ | ======================= ]\n";
-   }
-    if (o_cerr_stream_enabled_) {
-      std::cout << ConsoleSettings::GetColorCode(ConsoleSettings::common_color) << "[ --- " << group_name
-                << " --- ] " << "[ v ======================= ]\n";
-      //  o_stream << "[ | ======================= ]\n";
+    if (OutputConfig::o_cout_stream_enabled) {
+      StdOstreamTest::DisplayGroupStartOstream(std::cout, group_name);
+    }
+    if (OutputConfig::o_cerr_stream_enabled) {
+      StdOstreamTest::DisplayGroupStartOstream(std::cerr, group_name);
     }
   }
 };
 
 TestResult TestIOManager::general_test_output_;
-bool TestIOManager::o_stream_enabled_ = true;
-bool TestIOManager::o_cerr_stream_enabled_ = false;
-bool TestIOManager::o_json_file_enabled_ = false;
